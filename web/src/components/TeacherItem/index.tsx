ebc2c0c06a86aa6d60da4a4756fd76b725b4812b
Gustavo Ferreira <gfmb94@gmail.com>
2020-08-09 22:02:49 -0300

    Finalizando o Front-End

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,36 +3,49 @@ import React from 'react';
 import './styles.css';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+import api from '../../services/api';
 
-function TeacherItem(){
+export interface Teacher{ 
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: number; 
+}
+
+interface TeacherItemProps{
+  teacher: Teacher;
+}
+
+
+const TeacherItem: React.FunctionComponent<TeacherItemProps> = ({teacher })=> {
+  function createNewConnection(){
+    api.post('/connections', {
+      user_id: teacher.id,
+    })
+  }
+
   return(
     <article className="teacher-item">
     <header>
-      <img src="https://ci5.googleusercontent.com/proxy/jP6YkOOJ3UYyjVsnWlnTbqj37iEz9RtaY8bhD3G07ZfTFb7DecFuJYiAltWQTrKVcA=s0-d-e1-ft#https://i.ibb.co/WBVdCBS/o.jpg" alt="Gustavo Ferreira"/>
+      <img src={teacher.avatar} alt={teacher.name}/>
       <div>
-        <strong>
-        Gustavo Ferreira
-        </strong>
-        <span>
-        Informatica
-        </span>
+        <strong>{teacher.name}</strong>
+        <span>{teacher.subject}</span>
       </div> 
     </header>
-    <p>
-      Desenvolvedor apaixonado por tecnologia e buscando sempre inovação.<br /><br />
-      Formado em Engenharia da computação e atualmente desenvolvedor Java e ReactNative.
-    </p>
+    <p>{teacher.bio}</p>
     <footer>
-      <p>
-        Preço/hora
-        <strong>
-          R$ 15,00
-        </strong>
-      </p>
-      <button type="button">
+      <p>Preço/hora<strong>{teacher.cost}</strong></p>
+      <a 
+        target="_blank" 
+        onClick={createNewConnection} 
+        href={`https://wa.me/${teacher.whatsapp}`}>
         <img src={whatsappIcon} alt="WhatsApp"/>
         Entrar em contato
-      </button>
+      </a>
     </footer>          
   </article>
   );
